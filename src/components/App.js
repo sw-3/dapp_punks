fc7e07dc0665fafdd3ff4c59c4c8ba72fe3688d0
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-04-11 21:29:46 -0500

    WIP: front end setup

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,21 @@ import Navigation from './Navigation';
 import Loading from './Loading';
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import NFT_ABI from '../abis/NFT.json'
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from '../config.json';
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [nft, setNFT] = useState(null)
+
   const [account, setAccount] = useState(null)
+
+  const [revealTime, setRevealTime] = useState(0)
+  const [maxSupply, setMaxSupply] = useState(0)
+  const [totalSupply, setTotalSupply] = useState(0)
+  const [cost, setCost] = useState(0)
   const [balance, setBalance] = useState(0)
 
   const [isLoading, setIsLoading] = useState(true)
@@ -21,16 +29,28 @@ function App() {
   const loadBlockchainData = async () => {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    // initiate contract
+    const nft = new ethers.Contract(config[31337].nft.address, NFT_ABI, provider)
+    setNFT(nft)
 
     // Fetch accounts
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
 
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
+    // fetch countdown
+    const allowMintingOn = await nft.allowMintingOn()
+    setRevealTime(allowMintingOn.toString() + '000')  // need miliseconds in js
+
+    // fetch contract data
+    setMaxSupply(await nft.maxSupply())
+    setTotalSupply(await nft.totalSupply())
+    setCost(await nft.cost())
+
+    // fetch account
+    setBalance(await nft.balanceOf(account))
 
     setIsLoading(false)
   }
@@ -45,13 +65,12 @@ function App() {
     <Container>
       <Navigation account={account} />
 
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
+      <h1 className='my-4 text-center'>Dapp Punks</h1>
 
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
           <p className='text-center'>Edit App.js to add your code here.</p>
         </>
       )}
