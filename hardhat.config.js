54cff2d92dc88fb7d948c12d1592da74dafb92bc
ScottW3 <77562115+sw-3@users.noreply.github.com>
2023-04-16 22:38:27 -0500

    adds test network config for deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    localhost: {},
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_GOERLI}`,
+      accounts: privateKeys.split(','),
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY_MUMBAI}`,
+      accounts: privateKeys.split(','),
+    }
+  },
 };
